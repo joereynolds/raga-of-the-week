6084a47d0ef0aa850022de1bfa1f37725b1cf8b2
Joe Reynolds <joereynolds952@gmail.com>
2024-04-29 23:32:54 +0100

    Add highlighting as track is playing

--- resources/views/raga.blade.php
+++ resources/views/raga.blade.php
@@ -118,19 +118,44 @@
 <script defer>
 
 (function() {
-    Tone.Transport.start()
   // I am sorry for appropriating your culture badly
   function playRaga(notes) {
+      Tone.Transport.stop().start();
+      highlightTableColumn(1)
       const synth = new Tone.PolySynth().toDestination();
       synth.triggerAttackRelease('C2', 4);
+
+      index = 1
+      loop = new Tone.Loop(time => {
+          highlightTableColumn(index);
+          index++
+          console.log(index);
+      }, 0.5);
+
+      loop.start().stop(5);
 
       let delay = Tone.now();
       for(let i = 0; i < notes.length; i++) {
           synth.triggerAttackRelease(notes[i] + '4', '8n', delay);
           delay += 0.5
       }
+
       // Play the first note an octave higher to simulate the 8th note
       synth.triggerAttackRelease(notes[0] + '5', '8n', delay);
+  }
+
+  function highlightTableColumn(columnNumber) {
+      document.querySelectorAll('table').forEach(table => {
+
+          table.querySelectorAll('td').forEach(td => {
+              td.classList.remove('highlighted');
+              console.log('here')
+              let index = [].indexOf.call(td.parentElement.children, td);
+              if (index === columnNumber) {
+                  td.classList.add('highlighted');
+              }
+          });
+      });
   }
 
   document.querySelectorAll("button").forEach(button => {
