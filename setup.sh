5ca754606383e93d09cc204e98a1f961ba623ea4
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2024-04-30 13:30:27 +0100

    Add 2 more ragas

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+# docker run --rm --interactive --tty  --volume $PWD:/app composer install
 docker-compose run --rm php php artisan migrate:fresh --seed
 docker-compose run --rm php php artisan app:find-similar-ragas
