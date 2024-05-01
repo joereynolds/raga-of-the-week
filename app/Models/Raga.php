4a69bb6044cc2c9cea4bcfd05505252480217b1a
Joe Reynolds <joereynolds952@gmail.com>
2024-05-01 22:30:05 +0100

    Add accessor for is_janya

--- app/Models/Raga.php
+++ app/Models/Raga.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -39,5 +40,12 @@ class Raga extends Model
     public function janya(): HasMany
     {
         return $this->hasMany(MelakartaJanyaLink::class);
+    }
+
+    protected function isJanya(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->id > 72
+        );
     }
 }
--- resources/views/raga.blade.php
+++ resources/views/raga.blade.php
@@ -83,27 +83,29 @@
 
                 </table>
 
-                <h3>Janya Ragas</h3>
-                <p>Janya ragas are ragas that are derived from the parent raga (in this case {{$raga->name}}).</p>
-                <p>Here are the Janya ragas for {{$raga->name}}</p>
-                <ul>
-                    @forelse($raga->janya as $janya)
-                        <li>
-                            <a
-                                href=" {{
-                                    route(
-                                        'raga',
-                                        ['id' => App\Models\Raga::find($janya->janya_id)->id]
-                                    )
-                                }}"
-                            >
-                                {{ App\Models\Raga::find($janya->janya_id)->name }}
-                            </a>
-                        </li>
-                    @empty
-                        <p>None found :(</p>
-                    @endforelse
-                </ul>
+                @if (!$raga->isJanya)
+                    <h3>Janya Ragas</h3>
+                    <p>Janya ragas are ragas that are derived from the parent raga ({{$raga->name}}).</p>
+                    <p>Here are the Janya ragas for {{$raga->name}}</p>
+                    <ul>
+                        @forelse($raga->janya as $janya)
+                            <li>
+                                <a
+                                    href=" {{
+                                        route(
+                                            'raga',
+                                            ['id' => App\Models\Raga::find($janya->janya_id)->id]
+                                        )
+                                    }}"
+                                >
+                                    {{ App\Models\Raga::find($janya->janya_id)->name }}
+                                </a>
+                            </li>
+                        @empty
+                            <p>None found :(</p>
+                        @endforelse
+                    </ul>
+                @endif
 
 
 
