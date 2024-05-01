6c82ba98a334dbb7350d548a6f1929a77f8d6349
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2024-05-01 21:18:33 +0100

    Restructure HTML

--- resources/views/raga.blade.php
+++ resources/views/raga.blade.php
@@ -12,107 +12,122 @@
     </head>
 
     <body>
-        <h1>Raga of the week</h1>
+        <main>
+            <h1>Raga of the week</h1>
 
-        <p>Welcome!</p>
-        <p>Raga of the week shows a different Melakarta raga each week*.</p>
-        <p>This week's <strong>raga of the week</strong> is...</p>
-        <small>*(Support for other ragas like Janyas is on the list)</small>
+            <p>Welcome!</p>
+            <p>Raga of the week shows a different Melakarta raga each week*.</p>
+            <p>This week's <strong>raga of the week</strong> is...</p>
 
-        @foreach($ragas as $raga)
+            @foreach($ragas as $raga)
 
-            <h2>{{ $raga->name }}</h2>
+                <h2>{{ $raga->name }}</h2>
 
-            <p>{{$raga->name}} is number {{$raga->number}} of the Melakarta ragas.</p>
+                <p>{{$raga->name}} is number {{$raga->number}} of the Melakarta ragas.</p>
 
 
-            <button
-                data-notes='[
-                    "{{$raga->notes->first}}",
-                    "{{$raga->notes->second}}",
-                    "{{$raga->notes->third}}",
-                    "{{$raga->notes->fourth}}",
-                    "{{$raga->notes->fifth}}",
-                    "{{$raga->notes->sixth}}",
-                    "{{$raga->notes->seventh}}"
-                ]'
-            >
-                Play Raga
-            </button>
+                <button
+                    data-notes='[
+                        "{{$raga->notes->first}}",
+                        "{{$raga->notes->second}}",
+                        "{{$raga->notes->third}}",
+                        "{{$raga->notes->fourth}}",
+                        "{{$raga->notes->fifth}}",
+                        "{{$raga->notes->sixth}}",
+                        "{{$raga->notes->seventh}}"
+                    ]'
+                >
+                    Play Raga
+                </button>
 
-            <table>
-                <tr>
-                    <td>Arohana</td>
-                    <td>{{$raga->arohana->shadja}}</td>
-                    <td>{{$raga->arohana->rishabha}}</td>
-                    <td>{{$raga->arohana->gandhara}}</td>
-                    <td>{{$raga->arohana->madhyama}}</td>
-                    <td>{{$raga->arohana->panchama}}</td>
-                    <td>{{$raga->arohana->dhaivata}}</td>
-                    <td>{{$raga->arohana->nishada}}</td>
-                </tr>
-                <tr>
-                    <td>Avarohana</td>
-                    <td>{{$raga->avarohana->shadja}}</td>
-                    <td>{{$raga->avarohana->nishada}}</td>
-                    <td>{{$raga->avarohana->dhaivata}}</td>
-                    <td>{{$raga->avarohana->panchama}}</td>
-                    <td>{{$raga->avarohana->madhyama}}</td>
-                    <td>{{$raga->avarohana->gandhara}}</td>
-                    <td>{{$raga->avarohana->rishabha}}</td>
-                </tr>
-                <tr>
-                    <td>Notes</td>
-                    <td>{{$raga->notes->first}}</td>
-                    <td>{{$raga->notes->second}}</td>
-                    <td>{{$raga->notes->third}}</td>
-                    <td>{{$raga->notes->fourth}}</td>
-                    <td>{{$raga->notes->fifth}}</td>
-                    <td>{{$raga->notes->sixth}}</td>
-                    <td>{{$raga->notes->seventh}}</td>
-                </tr>
-                <tr>
-                    <td>Formula</td>
-                    <td>{{$raga->formula->first}}</td>
-                    <td>{{$raga->formula->second}}</td>
-                    <td>{{$raga->formula->third}}</td>
-                    <td>{{$raga->formula->fourth}}</td>
-                    <td>{{$raga->formula->fifth}}</td>
-                    <td>{{$raga->formula->sixth}}</td>
-                    <td>{{$raga->formula->seventh}}</td>
-                </tr>
+                <table>
+                    <tr>
+                        <td>Arohana</td>
+                        <td>{{$raga->arohana->shadja}}</td>
+                        <td>{{$raga->arohana->rishabha}}</td>
+                        <td>{{$raga->arohana->gandhara}}</td>
+                        <td>{{$raga->arohana->madhyama}}</td>
+                        <td>{{$raga->arohana->panchama}}</td>
+                        <td>{{$raga->arohana->dhaivata}}</td>
+                        <td>{{$raga->arohana->nishada}}</td>
+                    </tr>
+                    <tr>
+                        <td>Avarohana</td>
+                        <td>{{$raga->avarohana->shadja}}</td>
+                        <td>{{$raga->avarohana->nishada}}</td>
+                        <td>{{$raga->avarohana->dhaivata}}</td>
+                        <td>{{$raga->avarohana->panchama}}</td>
+                        <td>{{$raga->avarohana->madhyama}}</td>
+                        <td>{{$raga->avarohana->gandhara}}</td>
+                        <td>{{$raga->avarohana->rishabha}}</td>
+                    </tr>
+                    <tr>
+                        <td>Notes</td>
+                        <td>{{$raga->notes->first}}</td>
+                        <td>{{$raga->notes->second}}</td>
+                        <td>{{$raga->notes->third}}</td>
+                        <td>{{$raga->notes->fourth}}</td>
+                        <td>{{$raga->notes->fifth}}</td>
+                        <td>{{$raga->notes->sixth}}</td>
+                        <td>{{$raga->notes->seventh}}</td>
+                    </tr>
+                    <tr>
+                        <td>Formula</td>
+                        <td>{{$raga->formula->first}}</td>
+                        <td>{{$raga->formula->second}}</td>
+                        <td>{{$raga->formula->third}}</td>
+                        <td>{{$raga->formula->fourth}}</td>
+                        <td>{{$raga->formula->fifth}}</td>
+                        <td>{{$raga->formula->sixth}}</td>
+                        <td>{{$raga->formula->seventh}}</td>
+                    </tr>
 
-            </table>
+                </table>
 
-            <p>
-                Similar ragas
+                <p>
+                    Similar ragas
 
-                <ul>
-                    @forelse($raga->similarRaga as $similarRaga)
-                        <li>
-                            <a
-                                href=" {{
-                                    route(
-                                        'raga',
-                                        ['id' => App\Models\Raga::find($similarRaga->linked_raga_id)->number]
-                                    )
-                                }}"
-                            >
-                                {{ App\Models\Raga::find($similarRaga->linked_raga_id)->name }}
-                            </a>
-                        </li>
-                    @empty
-                        <p>None found :(</p>
-                    @endforelse
-                </ul>
-            </p>
-        @endforeach
+                    <ul>
+                        @forelse($raga->similarRaga as $similarRaga)
+                            <li>
+                                <a
+                                    href=" {{
+                                        route(
+                                            'raga',
+                                            ['id' => App\Models\Raga::find($similarRaga->linked_raga_id)->number]
+                                        )
+                                    }}"
+                                >
+                                    {{ App\Models\Raga::find($similarRaga->linked_raga_id)->name }}
+                                </a>
+                            </li>
+                        @empty
+                            <p>None found :(</p>
+                        @endforelse
+                    </ul>
+                </p>
+            @endforeach
+
+        </main>
 
         <footer>
             <div>
+                <a href="">Previous</a>
+            </div>
+
+            <div>
+                <a href="">Next</a>
+            </div>
+
+            <div>
                 <a href="{{ route('random') }}">Random Raga</a>
             </div>
-            <small>Corrections? Please <a>Email me</a></small>
+
+            <div>
+                <small>Corrections? Please <a>Email me</a></small>
+            </div>
+
+            <small>*(Support for other ragas like Janyas is on the list)</small>
         </footer>
 
     </body>
