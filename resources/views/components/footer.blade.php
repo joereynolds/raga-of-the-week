837e5cf86da5fbb51628682523563e89d7b02107
Joe Reynolds <joereynolds952@gmail.com>
2024-05-03 08:39:09 +0100

    Style footer links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,26 @@
 <footer>
 
-    @if ($previousRagaId)
-        <div>
-            <a
-                href="{{ route('raga', ['id' => $previousRagaId])}}"
-            >
-                Previous
-            </a>
-        </div>
-    @endif
+    <div class="flex-container">
+        @if ($previousRagaId)
+            <div>
+                <a href="{{ route('raga', ['id' => $previousRagaId])}}" >
+                    Previous
+                </a>
+            </div>
+        @endif
 
-    @if ($nextRagaId)
-        <div>
-            <a
-                href="{{ route('raga', ['id' => $nextRagaId])}}"
-            >
-                Next
-            </a>
+        <div class="text-align-center">
+            <a href="{{ route('random') }}">Random Raga</a>
         </div>
-    @endif
 
-    <div>
-        <a href="{{ route('random') }}">Random Raga</a>
+        @if ($nextRagaId)
+            <div class="text-align-right">
+                <a href="{{ route('raga', ['id' => $nextRagaId])}}" >
+                    Next
+                </a>
+            </div>
+        @endif
+
     </div>
 
     <div>
