143aab929d80f854da0671568de632fc8a21e182
Joe Reynolds <joereynolds952@gmail.com>
2024-05-16 11:28:46 +0100

    Hide most similar ragas if there's loads of them

--- resources/views/components/similar-ragas.blade.php
+++ resources/views/components/similar-ragas.blade.php
@@ -3,7 +3,7 @@
 <p>Here's a list of closely related ragas</p>
 
 <ul>
-    @forelse($raga->similarRaga as $similarRaga)
+    @forelse($raga->similarRaga->take(5) as $similarRaga)
         <li>
             <a href=" {{ route('raga', ['id' => $similarRaga->raga->id]) }}">
                 {{ $similarRaga->raga->name }}
@@ -29,3 +29,34 @@
         <p>None found :(</p>
     @endforelse
 </ul>
+
+@if ($raga->similarRaga->count() > 5)
+    <details>
+        <summary>See more</summary>
+            <ul>
+                @foreach($raga->similarRaga->skip(5) as $similarRaga)
+                        <li>
+                            <a href=" {{ route('raga', ['id' => $similarRaga->raga->id]) }}">
+                                {{ $similarRaga->raga->name }}
+                            </a>
+                            @php
+                                $thisRagasNotes = [];
+                                foreach ($raga->arohana as $arohana) {
+                                    $thisRagasNotes[] = $arohana->swara->note;
+                                }
+
+                                $otherRagasNotes = [];
+                                foreach ($similarRaga->raga->arohana as $arohana) {
+                                    $otherRagasNotes[] = $arohana->swara->note;
+                                }
+
+                                $thisRagaDiff  = current(array_diff($thisRagasNotes, $otherRagasNotes));
+                                $otherRagaDiff = current(array_diff($otherRagasNotes, $thisRagasNotes));
+
+                            @endphp
+                            (Has <strong>{{ $otherRagaDiff }}</strong> instead of <strong>{{ $thisRagaDiff}}</strong>)
+                        </li>
+                @endforeach
+            </ul>
+    </details>
+@endif
