3a29c2096add7913ca2a3e3a4557e543ce8d9a40
Joe Reynolds <joereynolds952@gmail.com>
2026-02-05 09:17:10 +0000

    Add icon for external link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -26,8 +26,8 @@
     <div class="flex justify-between mt-4">
         <div>
             <a
+                class="cursor-pointer"
                 hx-on:click="document.getElementById('issue').classList.toggle('hidden')"
-                href="#"
             >
                 Raise an issue
             </a>
@@ -40,8 +40,27 @@
         </div>
 
         <div>
-            <a href="https://github.com/joereynolds/raga-of-the-week">
+            <a
+                href="https://github.com/joereynolds/raga-of-the-week"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 See the code
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4 inline"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                    />
+                </svg>
+
             </a>
         </div>
     </div>
