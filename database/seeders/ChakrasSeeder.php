5c0d86c958dedf8a29c03e9661c819b5d6c5a2fe
Joe Reynolds <joereynolds952@gmail.com>
2026-08-26 08:34:43 +0100

    Fix seeder name

--- database/seeders/ChakrasSeeder.php
+++ database/seeders/ChakrasSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class Chakras extends Seeder
+class ChakrasSeeder extends Seeder
 {
     public function run(): void
     {
